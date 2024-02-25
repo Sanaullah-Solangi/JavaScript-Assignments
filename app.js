6a586df124dc2eea3c58651397753183d38baf47
Sanaullah <sanaullahsolangi817@gmail.com>
2024-02-25 18:28:02 +0500

    comment color is added

--- app.js
+++ app.js
@@ -1,107 +1,107 @@
 //*=========== Chapter 1 ( Q 1) ===========
 
-// alert("Hello Users");
+//? alert("Hello Users");
 
 //*=========== Chapter 1 ( Q 2) ===========
 
-// alert("Error! Please enter a valid password");
+//? alert("Error! Please enter a valid password");
 
 //*=========== Chapter 1 ( Q 3) ===========
 
-// alert("Welcome to JS Land...\nHappy Coding!");
+//? alert("Welcome to JS Land...\nHappy Coding!");
 
 //*=========== Chapter 1 ( Q 4) ===========
 
-// alert("Welcome to JS Land...");
-// alert("Happy Coding\nPrevent this page from creating additional dialogs.");
+//? alert("Welcome to JS Land...");
+//? alert("Happy Coding\nPrevent this page from creating additional dialogs.");
 
 //*=========== Chapter 1 ( Q 5) ===========
 
-// console.log(alert("Welcome to JS Land..."));
+//? console.log(alert("Welcome to JS Land..."));
 
 //*=========== Chapter 1 ( Q 6) ===========
 
-// console.log(alert("Welcome to JS Land..."));
+//? console.log(alert("Welcome to JS Land..."));
 
 //!============ chapter 1 ends ============
 
 //*=========== Chapter 2 ( Q 1) ===========
 
-// var username
+//? var username
 
 //*=========== Chapter 2 ( Q 2) ===========
 
-// var myName = "SanaUllah"
+//? var myName = "SanaUllah"
 
 //*=========== Chapter 2 ( Q 3) ===========
 
-// var message = "Hello world";
-// alert(message);
+//? var message = "Hello world";
+//? alert(message);
 
 //*=========== Chapter 2 ( Q 4) ===========
 
-// var studentName = "Jhone Doe";
-// alert(studentName);
+//? var studentName = "Jhone Doe";
+//? alert(studentName);
 
-// var studentAge = "15 years old";
-// alert(studenAge);
+//? var studentAge = "15 years old";
+//? alert(studenAge);
 
-// var Certification = "Certified Mobile Application Deveopment";
-// alert(Certification);
+//? var Certification = "Certified Mobile Application Deveopment";
+//? alert(Certification);
 
 //*=========== Chapter 2 ( Q 5) ===========
 
-// var pizza = "PIZZA\nPIZZ\nPIZ\nPI\nP";
-// alert(pizza);
+//? var pizza = "PIZZA\nPIZZ\nPIZ\nPI\nP";
+//? alert(pizza);
 
 //*=========== Chapter 2 ( Q 6) ===========
 
-// var email = "sanaullahsolangi@gmail.com";
-// alert("My Email address is" + " " + email);
+//? var email = "sanaullahsolangi@gmail.com";
+//? alert("My Email address is" + " " + email);
 
 //*=========== Chapter 2 ( Q 7) ===========
 
-// var book = "A smarter way to learn JavaScript";
-// alert("I am trying to learn from the Book" + " " + book);
+//? var book = "A smarter way to learn JavaScript";
+//? alert("I am trying to learn from the Book" + " " + book);
 
 //*=========== Chapter 2 ( Q 8) ===========
 
-// document.write(
-//   "Yah! I can write HTML content through <strong>JavaScript</strong> "
-// );
+//? document.write(
+//?   "Yah! I can write HTML content through <strong>JavaScript</strong> "
+//? );
 
 //*=========== Chapter 2 ( Q 9) ===========
 
-// var design = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
-// alert(design);
+//? var design = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+//? alert(design);
 
 //!============ chapter 2 ends ============
 
 //*=========== Chapter 3 ( Q 1) ===========
 
-// var age = "I am 23 years old";
-// alert(age);
+//? var age = "I am 23 years old";
+//? alert(age);
 
 //*=========== Chapter 3 ( Q 2) ===========
 
-// var count = "5";
-// alert("You have visited" + " " + count + " " + "times");
+//? var count = "5";
+//? alert("You have visited" + " " + count + " " + "times");
 
 //*=========== Chapter 3 ( Q 3) ===========
 
-// var birthYear = "5";
-// document.write(
-//   "My birth year" +
-//     " " +
-//     birthYear +
-//     "<br> Data type of my declared variable is number"
-// );
+//? var birthYear = "5";
+//? document.write(
+//?   "My birth year" +
+//?     " " +
+//?     birthYear +
+//?     "<br> Data type of my declared variable is number"
+//? );
 
 //*=========== Chapter 3 ( Q 4) ===========
 
-// var visiterName = prompt("Enter your Name");
+//? var visiterName = prompt("Enter your Name");
 
-// var ProductTitle = prompt("Enter Product Name");
+//? var ProductTitle = prompt("Enter Product Name");
 
-// var quantity = prompt("Quantity you want");
-// document.write(visiterName + " " + quantity + " " + quantity);
+//? var quantity = prompt("Quantity you want");
+//? document.write(visiterName + " " + quantity + " " + quantity);
